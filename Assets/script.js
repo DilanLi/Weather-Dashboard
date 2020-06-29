ef2fd9990c86489b58dfb0a165bd658540acb77c
Dilan Li <dilanli0706@gmail.com>
2020-06-29 00:13:05 -0600

    added functionality to favorite cities when clicked

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
     
+    var city;
     //use moment.js library to display day of week and date
     var currentDay = moment().format('MMMM Do YYYY');
-    var currentTime = moment().format('h:mm a');
+    // var currentTime = moment().format('h:mm a');
     var currentDayOfWeek = moment().format("dddd");
     var currentInfo = currentDayOfWeek + ", " + currentDay;
     $("#date").text(currentInfo);
-    $("#current-time").text(currentTime);
+    // $("#current-time").text(currentTime);
     
-    //this function dynamically updates hours and seconds on the html
-    function updateTime(){
-        currentTime = moment().format('h:mm a');
-        $("#current-time").text(currentTime);
-    }
-    setInterval(updateTime, 59000);
 
+    //this function dynamically updates hours and seconds on the html
+    // function updateTime(){
+    //     currentTime = moment().format('h:mm a');
+    //     $("#current-time").text(currentTime);
+    // }
+    // setInterval(updateTime, 59000);
+
+    displayHistory();
     getWeather();
 
     function getWeather(){
-      var city = "salt lake city";
+      if (city !== ""){
+        localStorage.setItem(city, city);
+        var liElm = $("<li>").text(city);
+        liElm.addClass("list-group-item");
+        $(".list-group").append(liElm);  
+      }
+
       queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=17ffeabcb0395a48b5f63a70619d8c8e"
     $.ajax({
         url: queryURL,
@@ -47,8 +56,20 @@ $(document).ready(function() {
           })
             .then(function(response) {
               console.log(response);
-              $("#uv-index").text("UV Index: " + response.current.uvi)
+              var uvIndex = response.current.uvi;
+              $("#uv-container").text("UV Index: ");
+              console.log(uvIndex);
+              $("#uv-index").text(uvIndex);
+              if (uvIndex < 6) {
+                $("#uv-index").css("backgroud-color", "green");
+              } else if (uvIndex < 8) {
+                $("#uv-index").css("backgroud-color", "yellow");
+              } else {
+                console.log("correct");
+                $("#uv-index").css("backgroud-color", "red");
+              }
 
+              $(".five-day-forecast").empty();
               for (i=1;i<=5;i++){
                 var timeStamp = response.daily[i].dt * 1000;
                 var dateLine = new Date(timeStamp);
@@ -78,13 +99,26 @@ $(document).ready(function() {
     });
     }
 
+    function displayHistory(){
+      for (let i = 0; i< localStorage.length; i++) {
+        var key = localStorage.key(i);
+        if (key !== "undefined") {
+          var cities = localStorage.getItem(key);
+          var liElm = $("<li>").text(cities);
+          liElm.addClass("list-group-item");
+          $(".list-group").append(liElm);  
+        }
+      }
+    }
 
     $("#search").click(function(){
       event.preventDefault();
-      var city = $("#city").val();
-      // localStorage.setItem("defaultCity", city);
-      // var city = localStorage.getItem("defaultCity");
+      city = $("#city").val();
       getWeather();
     })
 
+    $(".list-group-item").click(function(){
+      city = $(this).text();
+      getWeather();
+    })
 });
